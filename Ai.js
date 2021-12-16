14bdfd8b152477ce42cca6178d20199143d6ec39
selvin Iraheta-Gutierrez <irahetaselvin9@gmail.com>
2021-12-15 19:02:42 -0500

    Finished ai class and export

---
+++ Ai.js
@@ -0,0 +1,15 @@
+"use strict"
+const {players} = require("./Player");
+
+class Ai extends Plyaer{
+    constructor(name,type){
+        super(name,type);
+        this.name;
+        this.type=Ai;
+    }
+    randomGesutures(gestures){
+        let randomGesutures=
+    }
+}
+
+module.exports={
---
+++ human.js
@@ -0,0 +1,34 @@
+"use strict"
+
+const {Playername} = require("./player");
+const prompt = require("prompt-sync");
+
+class Human extends Player{
+    constructor(name,type){
+        super(name,type);
+        this.name;
+        this.type=Human;
+    }
+    pickGenstures(){
+let userInput=prompt("What gesture would you like to use?");
+userInput;
+if(userInput='Rock'){
+    return userInput
+}
+else if(userInput='Scissors'){
+    return userInput;
+}
+else if(userInput='Paper'){
+return userInput;
+}
+else if(userInput='Lizard'){
+return userInput;
+}
+else if(userInput='Spock'){
+    return userInput;
+}
+else {
+    this.pickGenstures();
+}
+    }
+}
