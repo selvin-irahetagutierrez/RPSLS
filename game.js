a9c1096cbdbf0a0b1f011ebd615af7d7d8afc701
selvin Iraheta-Gutierrez <irahetaselvin9@gmail.com>
2021-12-20 10:16:54 -0500

    Game.js Game class

--- game.js
+++ game.js
@@ -1,2 +1,32 @@
-"use strict";
-console.log
+
+const {Human} = require("./human");
+const {Ai}=require ("./Ai");
+const Player = require("./Player");
+const prompt = require("prompt-sync")();
+
+
+class Game {
+    constructor(){
+        this.HumanOne = new Human('Selvin', Human);
+        this.HumanTwo = new Human('Ethan',Human);
+        this.AiPlahyer=new Ai('Angel',Ai)
+    }
+
+runGame() {
+    this.displayRules();
+    let userInput=prompt('Do you want to play against computer?');
+
+    switch(userInput){
+        case "Human":
+            this.playHuman();
+            break;
+        case "Ai":
+            this.playAi();
+            break;
+        default:
+        return this.runGame();
+    }
+    this.displayGameWinner();
+}
+
+}
