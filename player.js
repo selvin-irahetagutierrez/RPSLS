c3498af35eabbe0f16dcce639b096cdbd7a99615
selvin Iraheta-Gutierrez <irahetaselvin9@gmail.com>
2021-12-20 19:48:59 -0500

    Added Edits into Player

--- player.js
+++ player.js
@@ -1,26 +1,35 @@
 "use strict";
+const {Game} = require("./Game");
+let game = new Game();
+game.runGame();
+white_check_mark
+eyes
+raised_hands
 
+
+
+
+
+"use strict";
 class Player{
-
     constructor(name,type){
         this.name = name;
         this.type = type;
-        this.gestures = ["Rock","Paper","Scissors","Lizard","Spock"];
-        this.score=0;
+        this.gestures = ["Rock", "Paper", "Scissors", "Lizard", "Spock"];
+        this.score = 0;
     }
-    promptFor(question, valid){
-        do{
-            var response= prompt(question).trim();
-        } while(!response || !valid(response));
-        return response
-    }
-    chars(userInput){
-        if (userInput == "Rock","Paper","Scissors","Lizard","Spock"){
-            return userInput.toLowerCase() == ""|| userInput.toLowerCase()=="";
-        }
-    }
-
+         promptFor(question, valid){
+            do{
+          var response = prompt(question).trim();
+        }    while(!response || !valid(response));
+            return response;
+      }
+      chars(userInput){
+          if( userInput == "Rock", "Paper", "Scissors" , "Lizard", "Spock"){
+        return userInput.toLowerCase() == "" || userInput.toLowerCase() == "";
+          }
+}
 }
 module.exports = {
-    Player:Player
+    Player: Player
 }
